29c9791469496d35e8608e4faa654aefc17c7ade
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-10-30 08:10:24 +0100

    Improving the initial tests

--- src/main/java/org/jab/microservices/router/MyRouter.java
+++ src/main/java/org/jab/microservices/router/MyRouter.java
@@ -2,25 +2,24 @@ package org.jab.microservices.router;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Slf4j
 public class MyRouter {
 
     public RouterFunction<ServerResponse> myRoutes(MyHandler myHandler) {
 
-        return route(
-                GET("/api/endpoint").and(accept(APPLICATION_JSON)),
-                    myHandler::getEndpoint).andRoute(
-                GET("/api/endpoint/level1").and(accept(APPLICATION_JSON)),
-                    myHandler::getLevel1).andRoute(
-                GET("/api/endpoint/level1/level2").and(accept(APPLICATION_JSON)),
-                    myHandler::getLevel2);
+        return RouterFunctions
+                .route(GET("/api/endpoint")
+                        .and(accept(APPLICATION_JSON)), myHandler::getEndpoint)
+                .andRoute(GET("/api/endpoint/level1")
+                        .and(accept(APPLICATION_JSON)), myHandler::getLevel1)
+                .andRoute(GET("/api/endpoint/level1/level2")
+                        .and(accept(APPLICATION_JSON)), myHandler::getLevel2);
     }
-
 }
